d82cd55df7330bb90b75e86a992b9b4dd9e9a361
Dvk2002 <dvk2002@yandex.ru>
2020-04-01 15:03:08 +0500

    homework_L5

---
+++ blogparse/spiders/avito.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.loader import ItemLoader
+from blogparse.items import AvitoRealEstateItem
+
+
+class AvitoSpider(scrapy.Spider):
+    name = 'avito'
+    allowed_domains = ['avito.ru','www.avito.ru']
+    start_urls = ['https://www.avito.ru/syzran/kvartiry/']
+
+    def parse(self, response):
+        for num in response.xpath('//div[@data-marker="pagination-button"]//span/text()'):
+            try:
+                tmp = int(num.get())
+                yield response.follow(f'/syzran/kvartiry/?p={tmp}', callback=self.parse)
+
+            except ValueError as e:
+                continue
+            except TypeError as e:
+                continue
+        for ads_url in response.css('div.item_table h3.snippet-title a.snippet-link::attr("href")'):
+            yield response.follow(ads_url, callback= self.ads_parse)
+
+    def ads_parse(self, response):
+        item = ItemLoader(AvitoRealEstateItem(), response)
+        item.add_value('url', response.url)
+        item.add_css('author_name', 'div.seller-info-name  a::text')
+        item.add_css('author_url', 'div.seller-info-name  a::attr(href)')
+        item.add_xpath('title','//span[@class = "title-info-title-text"]/text()')
+        item.add_xpath('data', "//div[@class= 'title-info-metadata-item-redesign']/text()")
+        for sel in response.xpath("//li[@class='item-params-list-item']"):
+            param_value = sel.xpath("./text()").getall()[1]
+            param = sel.xpath(".//span/text()").get()
+            item.add_value('item_param', {param:param_value})
+        item.add_xpath('photos', "//div[contains(@class, 'gallery-img-frame')]/@data-url")
+        yield item.load_item()
+        print(1)
